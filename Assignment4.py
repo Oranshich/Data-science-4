dc84a260ef99decb9ec8cb6783f8723f37e3fcc7
Oran <shichman@post.bgu.ac.il>
2020-06-08 18:43:43 +0300

    initializing

---
+++ Assignment4.py
@@ -0,0 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.cluster import KMeans
+
+
+path = '../../Documents/University/Third year/Semester F/Data Science/Assignment4/Dataset.xlsx'
+df =pd.read_excel(path)
