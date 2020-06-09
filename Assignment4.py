417c872a72bc8c40a925592776ae66566fb57e87
Oran <shichman@post.bgu.ac.il>
2020-06-09 17:21:15 +0300

    starting

--- Assignment4.py
+++ Assignment4.py
@@ -1,17 +1,16 @@
-import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
-from sklearn.cluster import KMeans
 import PreProccesing as pp
+import ModelCreation as mc
 import GUI
 # Global Vars
 path = './Dataset.csv'
 
-# df = pd.read_csv(path)
-#     # Begin the PreProcessing
-#     pre_proc = pp.pre_processing(df)
-#     print("Preprocessing completed successfully!")
+df = pd.read_csv(path)
+# Begin the PreProcessing
+pre_proc = pp.pre_processing(df)
+mc.model(pre_proc)
+print("Preprocessing completed successfully!")
 
-if __name__ == '__main__':
-    our_root_window = GUI.Root()
-    our_root_window.mainloop()
+# if __name__ == '__main__':
+#     our_root_window = GUI.Root()
+#     our_root_window.mainloop()
