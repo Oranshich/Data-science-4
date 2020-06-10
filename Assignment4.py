0c85e919076d82ad245d833dc22cd5276dfa0de6
Oran <shichman@post.bgu.ac.il>
2020-06-10 09:44:11 +0300

    3.2

--- Assignment4.py
+++ Assignment4.py
@@ -3,11 +3,11 @@ import PreProccesing as pp
 import ModelCreation as mc
 import GUI
 
-df = pd.read_csv(path)
+df = pd.read_csv('Dataset.csv')
 # Begin the PreProcessing
-pre_proc = pp.pre_processing(df)
-mc.model(pre_proc)
+pre_proc = pp.pre_process(df)
 print("Preprocessing completed successfully!")
+mc.model(pre_proc)
 
 if __name__ == '__main__':
     our_root_window = GUI.Root()
--- ModelCreation.py
+++ ModelCreation.py
@@ -4,12 +4,12 @@ from sklearn.cluster import KMeans
 
 
 def model(pre_proc):
-    print(pre_proc['Social support'])
-    wcss = []
 
+    wcss = []
+    X = pre_proc.iloc[:, [2, 5]].values
     for i in range(1, 11):
         kmeans = KMeans(n_clusters=i, init='k-means++', max_iter=300, n_init=10, random_state=0)
-        kmeans.fit(pre_proc)
+        kmeans.fit(X)
         wcss.append(kmeans.inertia_)
     # plt.plot(range(1, 11), wcss)
     # plt.title('Elbow Method')
@@ -18,17 +18,19 @@ def model(pre_proc):
     # plt.show()
     #
     kmeans = KMeans(n_clusters=3, init='k-means++', max_iter=300, n_init=10, random_state=0)
-    y_kmeans = kmeans.fit_predict(pre_proc)
+    y_kmeans = kmeans.fit_predict(X)
     pre_proc['Cluster'] = y_kmeans
     # plt.scatter(pre_proc['Social support'], pre_proc['Generosity'])
-    X = pre_proc.iloc[:, [2, 5]].values
-    print(X)
+
     plt.scatter(X[y_kmeans == 0, 0], X[y_kmeans == 0, 1], s=100, c='red', label='Cluster 1')
     plt.scatter(X[y_kmeans == 1, 0], X[y_kmeans == 1, 1], s=100, c='blue', label='Cluster 2')
     plt.scatter(X[y_kmeans == 2, 0], X[y_kmeans == 2, 1], s=100, c='green', label='Cluster 3')
     plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], s=300, c='yellow')
+    plt.title('Generosity as dependent in Social Support attribute')
+    plt.xlabel('Social support')
+    plt.ylabel('Generosity')
     # plt.scatter(pre_proc['Social support'], pre_proc['Generosity'], s=100, c='red', label='Cluster 1')
     # plt.scatter(pre_proc[pred_y == 1, 0], pre_proc[pred_y == 1, 1], s=100, c='blue', label='Cluster 2')
     # plt.scatter(pre_proc[pred_y == 2, 0], pre_proc[pred_y == 2, 1], s=100, c='green', label='Cluster 3')
-    #plt.show()
+    plt.show()
     return None
