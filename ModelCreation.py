0acf1e8380d82879aca9279d31b40e9e6e74e2dd
Oran <shichman@post.bgu.ac.il>
2020-06-16 11:28:17 +0300

    3.2

--- ModelCreation.py
+++ ModelCreation.py
@@ -3,15 +3,16 @@ import matplotlib
 import matplotlib.pyplot as plt
 from matplotlib.text import Text
 from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
 matplotlib.use("TkAgg")
 
 
 def model(pre_proc):
     X = pre_proc.iloc[:, [2, 5]].values
-    for i in range(1, 11):
-        kmeans = KMeans(n_clusters=i, init='k-means++', max_iter=300, n_init=10, random_state=0)
-        kmeans.fit(X)
-        wcss.append(kmeans.inertia_)
+    # for i in range(1, 11):
+    #     kmeans = KMeans(n_clusters=i, init='k-means++', max_iter=300, n_init=10, random_state=0)
+    #     kmeans.fit(X)
+    #     wcss.append(kmeans.inertia_)
     # plt.plot(range(1, 11), wcss)
     # plt.title('Elbow Method')
     # plt.xlabel('Number of clusters')
@@ -21,12 +22,12 @@ def model(pre_proc):
     kmeans = KMeans(n_clusters=3, init='k-means++', max_iter=300, n_init=10, random_state=0)
     y_kmeans = kmeans.fit_predict(pre_proc)
     pre_proc['Cluster'] = y_kmeans
-
+    print(pre_proc)
     return X, y_kmeans, kmeans
 
 
 def get_plot(X, y_kmeans, kmeans, figure):
-    reduced_data = PCA(n_components=2).fit_transform(pre_proc)
+    #reduced_data = PCA(n_components=2).fit_transform(X)
     figure.scatter(X[y_kmeans == 0, 0], X[y_kmeans == 0, 1], s=100, c='red', label='Cluster 1')
     figure.scatter(X[y_kmeans == 1, 0], X[y_kmeans == 1, 1], s=100, c='blue', label='Cluster 2')
     figure.scatter(X[y_kmeans == 2, 0], X[y_kmeans == 2, 1], s=100, c='green', label='Cluster 3')
