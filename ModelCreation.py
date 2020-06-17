b55dd19cf6e9d5ccf6d66d78c0c41377f295d996
Oran <shichman@post.bgu.ac.il>
2020-06-17 13:20:49 +0300

    Choropleth

--- ModelCreation.py
+++ ModelCreation.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib
+import matplotlib.cm as cm
 import matplotlib.pyplot as plt
 from matplotlib.text import Text
 from sklearn.cluster import KMeans
@@ -11,31 +12,26 @@ matplotlib.use("TkAgg")
 
 
 def model(pre_proc):
-    X = pre_proc.iloc[:, [2, 5]].values
-    kmeansData = pre_proc.iloc[:, 1:15]
-    # for i in range(1, 11):
-    #     kmeans = KMeans(n_clusters=i, init='k-means++', max_iter=300, n_init=10, random_state=0)
-    #     kmeans.fit(X)
-    #     wcss.append(kmeans.inertia_)
-    # plt.plot(range(1, 11), wcss)
-    # plt.title('Elbow Method')
-    # plt.xlabel('Number of clusters')
-    # plt.ylabel('WCSS')
-    # plt.show()
-    #
+    X = pre_proc.iloc[:, [3, 6]].values
+    kmeansData = pre_proc.iloc[:, 1::]
+
+
     kmeans = KMeans(n_clusters=3, init='k-means++', max_iter=300, n_init=10, random_state=0)
     # print(pre_proc.dtypes())
     y_kmeans = kmeans.fit_predict(kmeansData)
     pre_proc['Cluster'] = y_kmeans
-    print(pre_proc.head())
     return X, y_kmeans, kmeans
 
 
 def get_plot(X, y_kmeans, kmeans, figure):
-    #reduced_data = PCA(n_components=2).fit_transform(X)
-    figure.scatter(X[y_kmeans == 0, 0], X[y_kmeans == 0, 1], s=100, c='red', label='Cluster 1')
-    figure.scatter(X[y_kmeans == 1, 0], X[y_kmeans == 1, 1], s=100, c='blue', label='Cluster 2')
-    figure.scatter(X[y_kmeans == 2, 0], X[y_kmeans == 2, 1], s=100, c='green', label='Cluster 3')
+    colors = cm.rainbow(np.linspace(0, 1, 3))
+    x = np.arange(10)
+    ys = [i + x + (i * x) ** 2 for i in range(10)]
+
+    for i in range(0, 3):
+        figure.scatter(X[y_kmeans == i, 0], X[y_kmeans == i, 1], s=100, c=colors[i], label='Cluster 1')
+    # figure.scatter(X[y_kmeans == 1, 0], X[y_kmeans == 1, 1], s=100, c='blue', label='Cluster 2')
+    # figure.scatter(X[y_kmeans == 2, 0], X[y_kmeans == 2, 1], s=100, c='green', label='Cluster 3')
     figure.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], s=300, c='yellow')
     figure.set_title('Generosity as dependent in Social Support attribute')
     figure.set_xlabel('Social support')
@@ -53,7 +49,6 @@ def extractCode(country, codes):
 def choropleth(pre_proc, path="."):
     path = path + "/"
     codes = pd.read_csv("countries_codes_and_coordinates.csv")
-    #pre_proc['code'] = codes[pre_proc['country']]
     pre_proc['code'] = pre_proc.apply(lambda row: extractCode(row['country'], codes), axis=1)
 
     fig = px.choropleth(pre_proc, locations="code",
