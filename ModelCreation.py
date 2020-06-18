06e4903d2d1e81295dd5f61eed2389fb63913d3d
Oran <shichman@post.bgu.ac.il>
2020-06-18 09:58:48 +0300

    Finishing

--- ModelCreation.py
+++ ModelCreation.py
@@ -12,13 +12,10 @@ import os
 matplotlib.use("TkAgg")
 
 
-def model(pre_proc, num_of_run=10, num_of_clusters=3):
+def model(pre_proc, num_of_clusters=3, num_of_run=10):
     X = pre_proc.iloc[:, [3, 6]].values
     kmeansData = pre_proc.iloc[:, 1::]
-
-
     kmeans = KMeans(n_clusters=num_of_clusters, init='k-means++', max_iter=300, n_init=num_of_run, random_state=0)
-    # print(pre_proc.dtypes())
     y_kmeans = kmeans.fit_predict(kmeansData)
     pre_proc['Cluster'] = y_kmeans
     return X, y_kmeans, kmeans
@@ -26,11 +23,9 @@ def model(pre_proc, num_of_run=10, num_of_clusters=3):
 
 def get_plot(X, y_kmeans, kmeans, figure, num_of_clusters=3):
     colors = cm.rainbow(np.linspace(0, 1, num=num_of_clusters))
-    x = np.arange(10)
-    ys = [i + x + (i * x) ** 2 for i in range(10)]
 
     for i in range(0, num_of_clusters):
-        figure.scatter(X[y_kmeans == i, 0], X[y_kmeans == i, 1], s=100, c=colors[i], label='Cluster 1')
+        figure.scatter(X[y_kmeans == i, 0], X[y_kmeans == i, 1], s=100, c=np.array([colors[i]]), label='Cluster 1')
     # figure.scatter(X[y_kmeans == 1, 0], X[y_kmeans == 1, 1], s=100, c='blue', label='Cluster 2')
     # figure.scatter(X[y_kmeans == 2, 0], X[y_kmeans == 2, 1], s=100, c='green', label='Cluster 3')
     figure.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], s=300, c='yellow')
@@ -54,7 +49,7 @@ def choropleth(pre_proc, path="."):
 
     fig = px.choropleth(pre_proc, locations="code",
                         color="Cluster",
-                        color_continuous_scale=px.colors.DEFAULT_PLOTLY_COLORS)
+                        color_continuous_scale=px.colors.sequential.haline)
     py.sign_in('rosengal', 'BCw4CTeuXUnQP4VOeozY')
     img_path = path + "map.png"
     if os.path.exists(img_path):
