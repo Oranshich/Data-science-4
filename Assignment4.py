44d3a07b5b23616b176a32d5fa2f29e98b6ed90d
Gal <galrosenthal@gmail.com>
2020-06-20 21:44:22 +0300

    cleaned the code

--- Assignment4.py
+++ Assignment4.py
@@ -1,13 +1,4 @@
-import pandas as pd
-import PreProccesing as pp
-import ModelCreation as mc
 import GUI
-
-# df = pd.read_csv('Dataset.csv')
-# # Begin the PreProcessing
-# pre_proc = pp.pre_process(df)
-# print("Preprocessing completed successfully!")
-# mc.model(pre_proc)
 
 if __name__ == '__main__':
     our_root_window = GUI.Root()
